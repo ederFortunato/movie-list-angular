8f4e08ae3b76fe88a247a28ce5467726d4cda07e
ederFortunato <eder.fortunato@gmail.com>
2013-08-27 17:30:07 -0300

    create first view, with search for movies

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/allmovies', {templateUrl: 'partials/allMovies.html', controller: 'AllMoviesCtrl'});
+    $routeProvider.when('/mymovies', {templateUrl: 'partials/myMovies.html', controller: 'MyMoviesCtrl'});
+    $routeProvider.otherwise({redirectTo: '/allmovies'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,38 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', ['localStorage']).
+  controller('AllMoviesCtrl',  ['$scope', '$http', '$store', function($scope, $http, $store) {
+  if($store.get('lsMyMovies') === null)
+    $store.set('lsMyMovies', {});
+
+    $scope.doSubmit = function(){
+      var urlserver = 'server/proxy.php?q=' + $scope.filterMovie  ;
+      $http.get(urlserver)
+        .success(function(data) {
+          $scope.movies = data;
+        })
+        .error(function(data, status, headers, config) {
+          //console.log('error http get');
+        });
+    };
+
+    $scope.getStatusMovie = function(idMovie){
+       var status = ['Not Watched', 'Watched', 'Want Watch'];
+       var lsMovies = $store.get('lsMyMovies');
+       var current = lsMovies[idMovie];
+       return current ? status[current] : status[0];
+    };
+
+    $scope.changeStatusMovie = function(idMovie, status){
+      var lsMovies = $store.get('lsMyMovies');
+      lsMovies[idMovie] = status;
+      $store.set('lsMyMovies', lsMovies);
+    };
 
   }])
-  .controller('MyCtrl2', [function() {
+
+  .controller('MyMoviesCtrl', [function() {
 
   }]);
+
